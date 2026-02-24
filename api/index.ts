2e37f86b69a98a49c2b6ea06d1ba3ac4b97a6127
S-P-H-E <sphe.g.personal@gmail.com>
2026-02-24 17:00:37 +0200

    adding favicon

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
+import { file } from 'bun'
 
 const app = new Elysia()
+
+app.get('/favicon.ico', () => file('./public/favicon.ico'))
 
 app.get("/", () => "Welcome to {{ project_name }}!")
 
